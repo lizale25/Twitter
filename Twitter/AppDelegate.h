ea069c6c44b755c9569da2c6714dd7d815a6c897
Lizbeth Alejandra Gonzalez <lizgonzalez@fb.com>
2018-07-02 13:46:14 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Lizbeth Alejandra Gonzalez on 7/2/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Lizbeth Alejandra Gonzalez on 7/2/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
